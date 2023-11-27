e1b133c78f6b34b38f17a5431ee8800634c9eaa1
rocco <rocco.reus@hva.nl>
2023-11-27 21:07:02 +0100

    helped fauzan with an easier way for verifying data

--- src/signup.ts
+++ src/signup.ts
@@ -1,104 +1,118 @@
-import { delay } from "./components/delay";
 import "./config";
-import { api, url } from "@hboictcloud/api";
-
-// Gets the variables from the outputs with the id username, email and password
-const firstnameInput: any = (document.getElementById("firstname") as HTMLInputElement);
-const lastnameInput: any = (document.getElementById("lastname") as HTMLInputElement);
-const usernameInput: any = (document.getElementById("username") as HTMLInputElement);
-const emailInput: any = (document.getElementById("email") as HTMLInputElement);
-const passwordInput: any = (document.getElementById("password") as HTMLInputElement);
-
-// Gets an array with emails from the database
-const emailData: any = await api.queryDatabase(
-    "SELECT email FROM user"
-);
-
-// keeps redirect message hidden
-document.getElementsByTagName("section")[0].setAttribute("style", "display:none");
+import {api} from "@hboictcloud/api";
+import {USER_QUERY} from "./query/user.query";
 
 
 /**
  * This method will be called when the page is loaded, this happens at the end of this file.
  */
 function setup(): void {
+    // keeps redirect message hidden
+    document.getElementsByTagName("section")[0].setAttribute("style", "display:none");
+    const firstnameInput: any = (document.getElementById("firstname") as HTMLInputElement);
+    const lastnameInput: any = (document.getElementById("lastname") as HTMLInputElement);
+    const usernameInput: any = (document.getElementById("username") as HTMLInputElement);
+    const emailInput: any = (document.getElementById("email") as HTMLInputElement);
+    const passwordInput: any = (document.getElementById("password") as HTMLInputElement);
 
-    // Regular Expression for names
-    // only letters are allowed and numbers are not allowed
-    const nameRegEx: RegExp = /^((?!\w\D+$){,45}.)/;
+    const submitButton: any = (document.getElementById("signupForm") as HTMLInputElement);
 
-    // Regular Expression for username
-    // Needs at least 5 alphanumerics and a limit of 40 alphanumerics 
-    const usernameRegEx: RegExp = /^((?![a-zA-Z0-9_]{5,45}).)*$/;
 
     // Regular Expression for email
-    // Needs alphanumerics before the @ which follows with a dot and 2-4 letters 
+    // Needs alphanumerics before the @ which follows with a dot and 2-4 letters
     const emailRegEx: RegExp = /^((?![\w-\.]+@([\w-]+\.)+[\w-]{2,4}).)*$/;
 
     // Regular Expression for password
     // Minimum ofeight and maximum 60 characters, at least one uppercase letter, one lowercase letter, one number and one special character
     const passwordRegEx: RegExp = /^((?!(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,60}$).)*$/;
 
-    // Creates an action for the login button, the code between the curly braces will be executed when the button gets clicked.
-    document.querySelector("#signupButton")?.addEventListener("click", async () => {
 
-        // loops through all the emails that exists in the database
-        for( const email of emailData) {
-            if (firstnameInput && lastnameInput && usernameInput && emailInput && passwordInput) {
-            // If the username input OR email input OR password input are empty
-            // Then an alert will be displayed which disappears in 3000 ms
-                if (firstnameInput.value === "" || lastnameInput.value === "" || usernameInput.value === "" || emailInput.value === "" || passwordInput.value === "" ) {
-                    const textInput: string = "There are empty fields!";
-                    return alertPopUp(textInput);
+    if (submitButton) {
+        submitButton.addEventListener("submit", async function (e): Promise<void> {
+            e.preventDefault();
+            console.log("submit");
+
+            document.getElementsByClassName("alert-danger")[0].setAttribute("style", "display: none");
+
+            const inputs: (HTMLInputElement | null)[] = [firstnameInput, lastnameInput, usernameInput, emailInput, passwordInput];
+
+            const verifiedInputs: boolean = await validateInputs(inputs);
+
+            if (verifiedInputs) {
+                const verifiedPass: boolean = await verifyPassword(passwordInput.value);
+                const verifiedEmail: boolean = await verifyEmail(emailInput.value);
+
+                if (verifiedPass && verifiedEmail) {
+                    try {
+                        const newUser: any = await signUpDatabase(firstnameInput.value, lastnameInput.value, usernameInput.value, emailInput.value, passwordInput.value);
+
+                        if (newUser) {
+                            console.log(newUser);
+                        }
+                    } catch (e) {
+                        console.log(e);
+                    }
                 }
-                // firstname validation
-                else if (firstnameInput.value.match(nameRegEx)) {
-                    const textInput: string = "Only alphabetic letters are allowed!";
-                    // Returns the alertPopUp function and with the assigned data
-                    return alertPopUp(textInput);
-                }
-                // lastname validation
-                else if (lastnameInput.value.match(nameRegEx)) {
-                    const textInput: string = "Only alphabetic letters are allowed!";
-                    // Returns the alertPopUp function and with the assigned data
-                    return alertPopUp(textInput);
-                }
-                // username validation
-                else if (usernameInput.value.match(usernameRegEx)) {
-                    const textInput: string = "Username needs at least 5 alphanumerics!";
-                    // Returns the alertPopUp function and with the assigned data
-                    return alertPopUp(textInput);
-                }
-                // email validation
-                else if (emailInput.value.match(emailRegEx)) {
-                    const textInput: string = "Your email does not exist!";
-                    // Returns the alertPopUp function and with the assigned data
-                    return alertPopUp(textInput);
-                }
-                // checks if email already exists using the for loop
-                else if (emailInput.value === email.email) {
-                    const textInput: string = "Your email already has an existing account!";
-                    // Returns the alertPopUp function and with the assigned data
-                    return alertPopUp(textInput);
-                }
-                // password validation
-                else if (passwordInput.value.match(passwordRegEx)) {
-                    const textInput: string = "Your password needs a minimum of eight characters, at least one uppercase letter, one lowercase letter, one number and one special character.";
-                    // Returns the alertPopUp function and with the assigned data
-                    return alertPopUp(textInput);
-                }
-                else {
-                // Calls the signUpDatabase function and sends the assigned data
-                    await signUpDatabase(firstnameInput.value, lastnameInput.value, usernameInput.value, emailInput.value, passwordInput.value);
-                    console.log("registered!");
-                    document.getElementsByTagName("main")[0].setAttribute("style", "display:none");
-                    document.getElementsByTagName("section")[0].setAttribute("style", "display:block");
-                    await delay(5000);
-                    url.redirect("login.html");
-                } 
-            };
+            }
+        });
+    }
+
+
+    /**
+     * Validates the input field and sets a custom validation message if needed.
+     * @param {HTMLInputElement | null} inputs - The input element to validate.
+     */
+    async function validateInputs(inputs): Promise<boolean> {
+        let noError: boolean = true;
+        for (const input of inputs) {
+            if (input && input.value === "") {
+                const textInput: string = input.name + " is required!";
+                // Returns the alertPopUp function and with the assigned data
+                await alertPopUp(textInput);
+                noError = false;
+            }
         }
-    });
+
+        return noError;
+    }
+
+    async function verifyEmail(email): Promise<boolean> {
+        if (email.match(emailRegEx)) {
+            const textInput: string = "Email is not correct!";
+            // Returns the alertPopUp function and with the assigned data
+            await alertPopUp(textInput);
+            return false;
+        }
+
+        try {
+            const emailExists: any = await api.queryDatabase(USER_QUERY.GET_EMAIL_BY_EMAIL, email);
+
+            if (emailExists[0]) {
+                const textInput: string = "Email already exists!";
+                // Returns the alertPopUp function and with the assigned data
+                await alertPopUp(textInput);
+            }
+
+
+            return !emailExists[0];
+
+        } catch (e) {
+            console.log(e);
+        }
+
+    }
+
+    async function verifyPassword(password): Promise<boolean> {
+        if (!password.match(passwordRegEx)) {
+            return true;
+        } else {
+            const textInput: string = "Your password needs a minimum of eight characters, at least one uppercase letter, one lowercase letter, one number and one special character.";
+            // Returns the alertPopUp function and with the assigned data
+            await alertPopUp(textInput);
+            return false;
+        }
+
+    }
 }
 
 
@@ -111,16 +125,12 @@ function setup(): void {
  * @param password
  * @returns Array with the user data
  */
-async function signUpDatabase(firstnameInput: string, lastnameInput: string, usernameInput: string, emailInput: string, passwordInput: string): Promise<Array<any> | undefined> {
+async function signUpDatabase(firstnameInput: string, lastnameInput: string, usernameInput: string, emailInput: string, passwordInput: string): Promise<any[] | string[]> {
 
     // tries to get the data out of the database
     try {
-        let dataString: string[] = [firstnameInput, lastnameInput, usernameInput, passwordInput, emailInput];
-        const data: any = await api.queryDatabase(
-            "INSERT INTO user (firstname, lastname, username, password, email) VALUES (?,?,?,?,?)",
-            ...dataString
-        );
-
+        const dataString: string[] = [firstnameInput, lastnameInput, usernameInput, passwordInput, emailInput];
+        const data: any = await api.queryDatabase(USER_QUERY.CREATE_USER, ...dataString);
         return data;
     } catch (error) {
         // if it fails, then it returns an empty array
@@ -129,14 +139,12 @@ async function signUpDatabase(firstnameInput: string, lastnameInput: string, use
 }
 
 /**
- * 
+ *
  * @param textInput
  */
 async function alertPopUp(textInput: string): Promise<void> {
     document.getElementsByClassName("alert-danger")[0].setAttribute("style", "display: block");
     document.getElementsByClassName("alert-danger")[0].innerHTML = textInput;
-    await delay(5000);
-    document.getElementsByClassName("alert-danger")[0].setAttribute("style", "display: none");
 }
 
 // Calls the setup function when the page is loaded
