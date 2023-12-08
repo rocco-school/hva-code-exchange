57365b36fd46f492324cb06d4a6213908348d975
Rocco Reus <rocco.reus@hva.nl>
2023-12-08 10:35:17 +0100

    added user session handler

---
+++ src/components/handleUserSession.ts
@@ -0,0 +1,14 @@
+import "../hboictcloud-config";
+import {session} from "@hboictcloud/api";
+
+
+/**
+ * Removes JWTToken from session upon
+ *
+ * @returns {Promise<void>} A Promise that resolves after the specified delay.
+ */
+export async function endUserSession(this: HTMLElement): Promise<void> {
+    // Remove JWTToken From session
+    session.remove("JWTToken");
+    location.reload();
+}
