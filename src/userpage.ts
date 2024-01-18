1c0e2e0025ae340c7cbc8689a9029caf16005b4c
rocco <rocco.reus@hva.nl>
2024-01-18 23:25:38 +0100

    add function to handle turning image into binary and binary into image

--- src/userpage.ts
+++ src/userpage.ts
@@ -7,7 +7,7 @@ import {security} from "./components/security";
 import {User} from "./models/user";
 import {initializeTagSelect} from "./components/initializeSelect";
 import {handleButtonClick} from "./components/customSelect";
-import {binaryDataToImage, imageToBinaryData} from "./components/handleProfilePicture";
+import {convertImageToBinary, uint8ArrayToBlob} from "./components/handleProfilePicture";
 
 // Asynchronous setup function
 async function setup(): Promise<void> {
@@ -55,62 +55,7 @@ async function setup(): Promise<void> {
 
 
     const file: HTMLInputElement = document.querySelector("#file") as HTMLInputElement;
-    const profilePicture: HTMLImageElement = document.querySelector(".profile-picture") as HTMLImageElement;
-
-    file.addEventListener("input", function (evt) {
-        const uploadedImage: HTMLInputElement = this as HTMLInputElement;
-
-        const selectedFile: any = uploadedImage.files![0];
-
-        console.log(selectedFile);
-
-        const fileReader: FileReader = new FileReader();
-
-        fileReader.onload = async function (): Promise<void> {
-            const dataUrl: any = fileReader.result;
-
-            // Create an image element
-            const image: HTMLImageElement = new Image();
-
-            image.onload = async function (): Promise<void> {
-                try {
-                    const binaryData: Uint8Array = await imageToBinaryData(image);
-                    console.log("Binary Data:", binaryData);
-
-                    // Create an image element from binary data
-                    const imageElementFromBinary: HTMLImageElement = await binaryDataToImage(binaryData, selectedFile.type);
-
-                    console.log("Binary data converted to image:", imageElementFromBinary);
-
-
-                    // Set the image as the source for profilePicture
-                    profilePicture.src = URL.createObjectURL(
-                        new Blob([binaryData.buffer], { type: selectedFile.type } /* (1) */)
-                    );
-
-                } catch (error) {
-                    console.error("Error fetching or displaying image:", error);
-                }
-
-            };
-
-            // Set the source of the image directly from the data URL
-            image.src = dataUrl;
-        };
-
-        // Read the file as a data URL
-        fileReader.readAsDataURL(selectedFile);
-    });
-
-// Function to read the image file and convert it to a Buffer
-    function convertImageToBuffer(imagePath) {
-        try {
-            return fs.readFileSync(imagePath);
-        } catch (error) {
-            console.error('Error reading the image file:', error);
-            return null;
-        }
-    }
+    // const profilePicture: HTMLImageElement = document.querySelector(".profile-picture") as HTMLImageElement;
 
     // Array of elements to be disabled during certain states
     const disabled: (HTMLInputElement | HTMLButtonElement | HTMLSelectElement | null)[] = [
@@ -119,20 +64,40 @@ async function setup(): Promise<void> {
         firstnameInput, lastnameInput, saveBtn, discardBtn
     ];
 
-    // Regular Expression for email
-    // Needs alphanumerics before the @ which follows with a dot and 2-4 letters
-    const emailRegEx: RegExp = /^((?![\w-\.]+@([\w-]+\.)+[\w-]{2,4}).)*$/;
-
-    // Regular Expression for password
-    // Minimum ofeight and maximum 60 characters, at least one uppercase letter, one lowercase letter, one number and one special character
-    const passwordRegEx: RegExp = /^((?!(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,60}$).)*$/;
-
     // Regular Expression for firstname and lastname
     const nameRegEx: RegExp = /^[a-zA-Z\s]+$/;
 
     const retrievedUser: any = await User.retrieveUser(loginStatus.userId);
     const retrievedUserTags: any = await User.getUserTags(loginStatus.userId);
-    const retrievedAllUserTags: any = await api.queryDatabase(USER_QUERY.GET_TAGS);
+
+    const user: User = new User(
+        retrievedUser.userId,
+        retrievedUser.firstname,
+        retrievedUser.lastname,
+        retrievedUser.dateOfBirth,
+        retrievedUser.username,
+        retrievedUser.experienceYears,
+        retrievedUser.profilePicture,
+        retrievedUser.password,
+        retrievedUser.email
+    );
+
+    file.addEventListener("input", async function (): Promise<void> {
+        const uploadedImage: HTMLInputElement = this as HTMLInputElement;
+
+        const selectedFile: File = uploadedImage.files![0];
+
+        const binaryImage: Uint8Array = await convertImageToBinary(selectedFile);
+
+        user.profilePicture = uint8ArrayToBlob(binaryImage);
+
+        console.log(uint8ArrayToBlob(binaryImage));
+        console.log(binaryImage);
+
+        console.log(user);
+
+        await user.updateUser();
+    });
 
     // Display user information in the UI
     if (usernameUser) {
@@ -227,9 +192,9 @@ async function setup(): Promise<void> {
             // TODO password comparison
             const emptyPasswordInputs: any = await checkPasswordValue(passwordInputs);
             if (!emptyPasswordInputs) return;
-            const verifiedNewPassword: boolean = await verifyNewPassword(newPasswordInput);
+            const verifiedNewPassword: boolean = await verifyNewPassword(newPasswordInput, errorPasswordMessageBox!);
             if (!verifiedNewPassword) return;
-            const verifiedConfirmPassword: boolean = await verifyConfirmPassword(confirmPasswordInput, newPasswordInput);
+            const verifiedConfirmPassword: boolean = await verifyConfirmPassword(confirmPasswordInput, newPasswordInput, errorPasswordMessageBox!);
             if (!verifiedConfirmPassword) return;
 
             if (emptyPasswordInputs && verifiedNewPassword && verifiedConfirmPassword) {
@@ -247,10 +212,6 @@ async function setup(): Promise<void> {
 
     if (saveBtn) {
         saveBtn.addEventListener("click", async (): Promise<void> => {
-            // const inputs: (HTMLInputElement | HTMLSelectElement | null)[] = [usernameInput, birthdayInput, programmingExperienceInput, emailInput, expertiseOptions, firstnameInput, lastnameInput];
-            // const verifiedInputs: any = checkValue(inputs);
-            const getTagIdExpertise: number = await retrieveTagId(expertiseOptions?.value);
-
             let questionTags: any[] = [];
 
             // Use the async function handleButtonClick when the submit button is clicked
@@ -274,9 +235,9 @@ async function setup(): Promise<void> {
 
             const verifiedEmail: boolean = await verifyEmail(emailInput);
             if (!verifiedEmail) return;
-            const verifiedFirstname: boolean = await verifyFirstname(firstnameInput);
+            const verifiedFirstname: boolean = await verifyFirstname(firstnameInput, nameRegEx);
             if (!verifiedFirstname) return;
-            const verifiedLastname: boolean = await verifyLastname(lastnameInput);
+            const verifiedLastname: boolean = await verifyLastname(lastnameInput, nameRegEx);
             if (!verifiedLastname) return;
 
             if (verifiedEmail && verifiedFirstname && verifiedLastname) {
@@ -301,130 +262,137 @@ async function setup(): Promise<void> {
         });
     }
 
-    async function retrieveTagId(tagName: string | undefined): Promise<any> {
-        return await api.queryDatabase(USER_QUERY.GET_TAG, tagName);
-    }
-
-    /**
-     * Validates the email field and sets a custom validation message if needed.
-     *
-     * @param email
-     */
-    async function verifyEmail(email: any): Promise<boolean> {
-        if (email.value.match(emailRegEx)) {
-            console.log(email.name + " format is not correct!");
-            // Returns the alertPopUp function and with the assigned data
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    async function verifyFirstname(firstname: any): Promise<boolean> {
-        if (!firstname.value.match(nameRegEx)) {
-            console.log(firstname.name + " format is not correct!");
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    async function verifyLastname(lastname: any): Promise<boolean> {
-        if (!lastname.value.match(nameRegEx)) {
-            console.log(lastname.name + " format is not correct!");
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    // Password validation functions
-
-    async function checkPasswordValue(inputs: (HTMLInputElement | null)[]): Promise<boolean> {
-        let valid: boolean = true;
-
-        inputs.forEach(inputPassword => {
-            if (inputPassword && inputPassword.value === "") {
-                console.log(inputPassword.name + " is empty!");
-                valid = false;
-            }
-        });
-
-        return valid;
-    }
-
-    async function verifyNewPassword(password: any): Promise<boolean> {
-        if (password.value.match(passwordRegEx)) {
-            errorPasswordMessageBox?.classList.remove("hidden");
-            if (errorPasswordMessageBox) {
-                errorPasswordMessageBox.innerHTML = "Your password needs a minimum of eight characters, at least one uppercase letter, one lowercase letter, one number, and one special character.";
-                setTimeout(() => {
-                    errorPasswordMessageBox.classList.add("hidden");
-                    errorPasswordMessageBox.innerHTML = "";
-                }, 5000);
-            }
-            return false;
-        } else {
-            return true;
-        }
-
-    }
-
-    // Validates the confirmation password and displays an error message if not matching
-    async function verifyConfirmPassword(password: any, newPassword: any): Promise<boolean> {
-        if (!password.value.match(newPassword.value)) {
-            // Show error message and hide after 5 seconds
-            errorPasswordMessageBox?.classList.remove("hidden");
-            if (errorPasswordMessageBox) {
-                errorPasswordMessageBox.innerHTML = "Password does not match!";
-                setTimeout(() => {
-                    errorPasswordMessageBox.classList.add("hidden");
-                    errorPasswordMessageBox.innerHTML = "";
-                }, 5000);
-            }
-            return false;
-        } else {
-            console.log("Password matches!");
-            return true;
-        }
-    }
-
-    // Updates user data in the database
-    async function updateUserData(username: string, dateOfBirth: any, programmingExperience: any, email: string | undefined, tagIds: any[], firstname: string | undefined, lastname: string | undefined, userId: number): Promise<void> {
-        const arrayUserData: any[] = [firstname, lastname, dateOfBirth, username, programmingExperience, email, userId];
-        console.log(arrayUserData);
-
-        // Update user data in the database
-        const userDatabase: Promise<any> = api.queryDatabase(USER_QUERY.UPDATE_USER, ...arrayUserData);
-
-        // Update user tags in the database
-        tagIds.forEach(tagId => {
-            const params: any[] = [userId, tagId];
-            const userTagDatabase: Promise<any> = api.queryDatabase(USER_QUERY.CREATE_USER_TAG, ...params);
-            console.log(userTagDatabase);
-        });
-
-        console.log(userDatabase);
-        return;
-    }
-
-    // Updates user password in the database
-    async function updatePasswordData(userId: number, updatedPassword: string): Promise<any> {
-        // Hash the new password
-        const hashedPassword: string | null = await hashPassword(updatedPassword);
-
-        // Handle hashing error
-        if (!hashedPassword) {
-            return new Error("Error hashing the password.");
-        }
-
-        // Update password in the database
-        const passwordDatabase: Promise<any> = api.queryDatabase(USER_QUERY.UPDATE_PASSWORD, hashedPassword, userId);
-
-        console.log(passwordDatabase);
-        console.log("SUCCESS! WOOHOO!");
-        return;
-    }
 }
 
 await setup();
+
+
+/**
+ * Validates the email field and sets a custom validation message if needed.
+ *
+ * @param email
+ */
+async function verifyEmail(email: any): Promise<boolean> {
+
+    // Regular Expression for email
+    // Needs alphanumerics before the @ which follows with a dot and 2-4 letters
+    const emailRegEx: RegExp = /^((?![\w-\.]+@([\w-]+\.)+[\w-]{2,4}).)*$/;
+
+    if (email.value.match(emailRegEx)) {
+        console.log(email.name + " format is not correct!");
+        // Returns the alertPopUp function and with the assigned data
+        return false;
+    } else {
+        return true;
+    }
+}
+
+async function verifyFirstname(firstname: any, nameRegEx: RegExp): Promise<boolean> {
+    if (!firstname.value.match(nameRegEx)) {
+        console.log(firstname.name + " format is not correct!");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+async function verifyLastname(lastname: any, nameRegEx: RegExp): Promise<boolean> {
+    if (!lastname.value.match(nameRegEx)) {
+        console.log(lastname.name + " format is not correct!");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// Password validation functions
+
+async function checkPasswordValue(inputs: (HTMLInputElement | null)[]): Promise<boolean> {
+    let valid: boolean = true;
+
+    inputs.forEach(inputPassword => {
+        if (inputPassword && inputPassword.value === "") {
+            console.log(inputPassword.name + " is empty!");
+            valid = false;
+        }
+    });
+
+    return valid;
+}
+
+async function verifyNewPassword(password: any, errorPasswordMessageBox: HTMLDivElement): Promise<boolean> {
+    // Regular Expression for password
+    // Minimum ofeight and maximum 60 characters, at least one uppercase letter, one lowercase letter, one number and one special character
+    const passwordRegEx: RegExp = /^((?!(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,60}$).)*$/;
+
+    if (password.value.match(passwordRegEx)) {
+        errorPasswordMessageBox?.classList.remove("hidden");
+        if (errorPasswordMessageBox) {
+            errorPasswordMessageBox.innerHTML = "Your password needs a minimum of eight characters, at least one uppercase letter, one lowercase letter, one number, and one special character.";
+            setTimeout(() => {
+                errorPasswordMessageBox.classList.add("hidden");
+                errorPasswordMessageBox.innerHTML = "";
+            }, 5000);
+        }
+        return false;
+    } else {
+        return true;
+    }
+
+}
+
+// Validates the confirmation password and displays an error message if not matching
+async function verifyConfirmPassword(password: any, newPassword: any, errorPasswordMessageBox: HTMLDivElement): Promise<boolean> {
+    if (!password.value.match(newPassword.value)) {
+        // Show error message and hide after 5 seconds
+        errorPasswordMessageBox?.classList.remove("hidden");
+        if (errorPasswordMessageBox) {
+            errorPasswordMessageBox.innerHTML = "Password does not match!";
+            setTimeout(() => {
+                errorPasswordMessageBox.classList.add("hidden");
+                errorPasswordMessageBox.innerHTML = "";
+            }, 5000);
+        }
+        return false;
+    } else {
+        console.log("Password matches!");
+        return true;
+    }
+}
+
+// Updates user data in the database
+async function updateUserData(username: string, dateOfBirth: any, programmingExperience: any, email: string | undefined, tagIds: any[], firstname: string | undefined, lastname: string | undefined, userId: number): Promise<void> {
+    const arrayUserData: any[] = [firstname, lastname, dateOfBirth, username, programmingExperience, email, userId];
+    console.log(arrayUserData);
+
+    // Update user data in the database
+    const userDatabase: Promise<any> = api.queryDatabase(USER_QUERY.UPDATE_USER, ...arrayUserData);
+
+    // Update user tags in the database
+    tagIds.forEach(tagId => {
+        const params: any[] = [userId, tagId];
+        const userTagDatabase: Promise<any> = api.queryDatabase(USER_QUERY.CREATE_USER_TAG, ...params);
+        console.log(userTagDatabase);
+    });
+
+    console.log(userDatabase);
+    return;
+}
+
+// Updates user password in the database
+async function updatePasswordData(userId: number, updatedPassword: string): Promise<any> {
+    // Hash the new password
+    const hashedPassword: string | null = await hashPassword(updatedPassword);
+
+    // Handle hashing error
+    if (!hashedPassword) {
+        return new Error("Error hashing the password.");
+    }
+
+    // Update password in the database
+    const passwordDatabase: Promise<any> = api.queryDatabase(USER_QUERY.UPDATE_PASSWORD, hashedPassword, userId);
+
+    console.log(passwordDatabase);
+    console.log("SUCCESS! WOOHOO!");
+    return;
+}
